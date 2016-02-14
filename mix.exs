5e2d7b8dd300765f69a43b916cd7fc8e93f3a8a3
Joshua Thompson <Jethom18@gmail.com>
2016-02-14 17:37:51 -0500

    Fix broken mix.exs

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Di.Mixfile do
     [app: :di,
      version: "0.1.0",
      elixir: "~> 1.2",
+     description: description,
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
